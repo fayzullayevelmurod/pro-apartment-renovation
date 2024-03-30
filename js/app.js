519fd4ee80c32f25d504d31584db641babee1bc2
Abdullayev Husniddin <husniddinabdullayev936@gmail.com>
2024-03-30 12:10:30 +0500

    project fineshed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,36 @@
 
-const mobileBtn = document.querySelector(".mobile-btn"),
-headerNav = document.querySelector(".header-nav");
+const menuBar = document.querySelector("#menu-bar"),
+headerNav = document.querySelector(".header-nav"),
+xMark = document.querySelector("#x-mark") ;
 
-mobileBtn.addEventListener('click', () => {
-  headerNav.classList.toggle('opacity')
+xMark.classList.remove('fa-xmark')
+
+
+menuBar.addEventListener('click', () => {
+  headerNav.classList.add('opacity');
+  menuBar.classList.remove("fa-bars");
+  xMark.classList.add("fa-xmark");
 })
+
+
+xMark.addEventListener('click', () => {
+  headerNav.classList.remove('opacity');
+  menuBar.classList.add("fa-bars");
+  xMark.classList.remove("fa-xmark");
+})
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if (accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+
+  });
+});
