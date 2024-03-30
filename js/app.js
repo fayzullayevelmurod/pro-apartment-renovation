f398ef59091e22a8d1928697fc775ca941f2bf64
Abdullayev Husniddin <husniddinabdullayev936@gmail.com>
2024-03-30 17:16:39 +0500

    ...

--- js/app.js
+++ js/app.js
@@ -4,16 +4,19 @@ const menuBar = document.querySelector("#menu-bar"),
   xMark = document.querySelector("#x-mark"),
   mobileClose = document.querySelector(".mobile-close"),
   contactBtn = document.querySelectorAll(".modalShouBtn"),
-  modalPege = document.querySelector(".modal-pege");
+  modalPege = document.querySelector(".modal-pege"),
+  body = document.querySelector("body");
 
 contactBtn.forEach(e => {
   e.addEventListener('click', () => {
-    modalPege.classList.add("z-index")
+    modalPege.classList.add("z-index");
+    body.style.overflow = 'hidden';
   })
 })
 
 mobileClose.addEventListener("click", () => {
   modalPege.classList.remove("z-index");
+  body.style.overflow = 'auto';
 }
 )
 
@@ -24,6 +27,7 @@ menuBar.addEventListener('click', () => {
   headerNav.classList.add('opacity');
   menuBar.classList.remove("fa-bars");
   xMark.classList.add("fa-xmark");
+  body.style.overflow = "hidden";
 })
 
 
@@ -31,6 +35,7 @@ xMark.addEventListener('click', () => {
   headerNav.classList.remove('opacity');
   menuBar.classList.add("fa-bars");
   xMark.classList.remove("fa-xmark");
+  body.style.overflow = "auto";
 })
 
 const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
